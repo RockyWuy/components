bdeb8a2d67de707656fa0f78af1f9b95e73c781f
RockyWuy <rockywuy@163.com>
2019-06-05 11:44:05 +0800

    初始化

---
+++ src/components/Carousel/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+interface IProps {
+    name: string;
+    age: number;
+}
+
+export class Carousel extends React.Component<IProps, {}> {
+    public render() {
+        return (
+            <div>sss</div>
+        );
+    }
+}
