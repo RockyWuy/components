19f189c79d387171ad7566d3e4a0faf2402cda6a
RockyWuy <rockywuy@163.com>
2019-09-26 16:11:59 +0800

    s

---
+++ src/router.tsx
@@ -0,0 +1,14 @@
+// import * as React from 'react';
+// import { BrowserRouter as Router, Switch, Route  } from 'react-router-dom';
+
+// export default class AppRouter extends React.Component<{}, {}> {
+//     public render() {
+//         return (
+//             <Router>
+//                 <Switch>
+//                     <Route exact path={'/'} component={<div>sss</div>} />
+//                 </Switch>
+//             </Router>
+//         );
+//     }
+// }
